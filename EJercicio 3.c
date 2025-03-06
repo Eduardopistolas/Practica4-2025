5d4be8f13e574862105d33d9a0bae76c54941d82
Eduardopistolas <eduardogranados0@comunidad.unam.mx>
2025-03-06 14:47:32 -0600

    Create EJercicio 3.c

---
+++ EJercicio 3.c
@@ -0,0 +1,29 @@
+    #include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int res = 0
+    int i;
+    
+    for (i=0; i < 1; i++)
+    {
+        res = res + i;
+    }
+    
+    printf("solucion 1 - EL numero es: %d\n",res);
+
+    res=0;
+    for (i=1; i <= 10 ; i++)
+    {
+        res=res + i 
+    }
+    printf("solucion 2 - EL numero es: %d\n",res);
+    res=0;
+    for (i=1; i <11;i++)
+    {
+    res=res+i;
+    }
+    rintf("solucion 4 - EL numero es: %d\n",res);
+    
+    return 0;
+    }
