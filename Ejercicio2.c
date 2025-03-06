257b06837e1859e01aa6e59af011a386acb261ed
Eduardopistolas <eduardogranados0@comunidad.unam.mx>
2025-03-06 14:30:15 -0600

    Create Ejercicio2.c

---
+++ Ejercicio2.c
@@ -0,0 +1,24 @@
+/Eduardo Granados
+//Ejercicio 2
+//PROGRAMA SI a>b
+
+#include <stdio.h>
+#include <stdlib.h>
+     int main()
+     {
+        int a= 3;
+        int b= 2;
+
+        if(a > b)
+        {
+            printf ("a es mayor\n");
+        }
+
+        else
+        {
+          printf ("b es mayor o igual a\n");
+    
+        }
+
+         return 0;
+     }
