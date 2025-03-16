c5cf946aacb22d1c33760501c74586ebaae3ccbe
Eduardopistolas <eduardogranados0@comunidad.unam.mx>
2025-03-15 21:07:34 -0600

    Update EJercicio 3.c

--- EJercicio 3.c
+++ EJercicio 3.c
@@ -1,29 +1,37 @@
-    #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
+
 int main()
 {
-    int res = 0
+    int res = 0;
     int i;
-    
-    for (i=0; i < 1; i++)
+
+    for (i = 0; i < 11; i++)
     {
         res = res + i;
     }
-    
-    printf("solucion 1 - EL numero es: %d\n",res);
+    printf("Solucion 1 - El numero es: %d\n", res);
 
-    res=0;
-    for (i=1; i <= 10 ; i++)
+    res = 0;
+    for (i = 1; i <= 10; i++)
     {
-        res=res + i 
+        res = res + i;
     }
-    printf("solucion 2 - EL numero es: %d\n",res);
-    res=0;
-    for (i=1; i <11;i++)
+    printf("Solucion 2 - El numero es: %d\n", res);
+
+    res = 0;
+    for (i = 0; i <= 10; i++)
     {
-    res=res+i;
+        res = res + i;
     }
-    rintf("solucion 4 - EL numero es: %d\n",res);
-    
+    printf("Solucion 3 - El numero es: %d\n", res);
+
+    res = 0;
+    for (i = 1; i < 11; i++)
+    {
+        res = res + i;
+    }
+    printf("Solucion 4 - El numero es: %d\n", res);
+
     return 0;
-    }
+}
